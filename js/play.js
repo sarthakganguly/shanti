dc5c670bdba9d68fb27874b442e389bc5332cc43
Sarthak Ganguly <sarthakganguly@users.noreply.github.com>
2020-10-18 00:55:05 +0530

    First build uploaded

---
+++ js/play.js
@@ -0,0 +1,90 @@
+function playRain()
+{
+    console.log("Playing the music");
+    var player = document.getElementById('player');
+    var source = document.getElementById('source');
+    source.src="./sounds/rain.mp3";
+    player.load();
+    player.play();
+    console.log("Can you hear it?")
+};
+function playFire()
+{
+    console.log("Playing the music");
+    var player = document.getElementById('player');
+    var source = document.getElementById('source');
+    source.src="./sounds/fire.mp3";
+    player.load();
+    player.play();
+    console.log("Can you hear it?")
+};
+function playGarden()
+{
+    console.log("Playing the music");
+    var player = document.getElementById('player');
+    var source = document.getElementById('source');
+    source.src="./sounds/garden.mp3";
+    player.load();
+    player.play();
+    console.log("Can you hear it?")
+};
+function playSeaside()
+{
+    console.log("Playing the music");
+    var player = document.getElementById('player');
+    var source = document.getElementById('source');
+    source.src="./sounds/seaside.mp3";
+    player.load();
+    player.play();
+    console.log("Can you hear it?")
+};
+function playStorm()
+{
+    console.log("Playing the music");
+    var player = document.getElementById('player');
+    var source = document.getElementById('source');
+    source.src="./sounds/storm.mp3";
+    player.load();
+    player.play();
+    console.log("Can you hear it?")
+};
+function playStormWithRain()
+{
+    console.log("Playing the music");
+    var player = document.getElementById('player');
+    var source = document.getElementById('source');
+    source.src="./sounds/storm-with-rain.mp3";
+    player.load();
+    player.play();
+    console.log("Can you hear it?")
+};
+function playSummerNight()
+{
+    console.log("Playing the music");
+    var player = document.getElementById('player');
+    var source = document.getElementById('source');
+    source.src="./sounds/summer-night.mp3";
+    player.load();
+    player.play();
+    console.log("Can you hear it?")
+};
+function playWaterStream()
+{
+    console.log("Playing the music");
+    var player = document.getElementById('player');
+    var source = document.getElementById('source');
+    source.src="./sounds/water-stream.mp3";
+    player.load();
+    player.play();
+    console.log("Can you hear it?")
+};
+function playWind()
+{
+    console.log("Playing the music");
+    var player = document.getElementById('player');
+    var source = document.getElementById('source');
+    source.src="./sounds/wind.mp3";
+    player.load();
+    player.play();
+    console.log("Can you hear it?")
+};
